7c3ba5e59c18cc0b741c3bd84734ff7a0c868c2b
jkhbuild <jkh.build@gmail.com>
2022-09-24 21:37:16 -0400

    battleship 2nd runthrough player

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
+    def get_move
+        print "enter a position with coordinates separated with a space like `4 7`"
+        string = gets.chomp
+
+        [string[0].to_i, string[-1].to_i]
+    end
 
 end
